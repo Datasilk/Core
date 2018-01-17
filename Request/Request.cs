f898e78fddc3183eb7a9a99b39b00ad73d85d926
Mark Entingh <entingh@gmail.com>
2018-01-17 10:03:57 -0800

    updated request errors

--- Request/Request.cs
+++ Request/Request.cs
@@ -22,13 +22,13 @@
         public string Error()
         {
             S.Response.StatusCode = 500;
-            return S.Server.LoadFileFromCache("/Pages/500.html"); ;
+            return S.Server.LoadFileFromCache("/Pages/500.html");
         }
 
         public string Error404()
         {
             S.Response.StatusCode = 404;
-            return S.Server.LoadFileFromCache("/Pages/404.html"); ;
+            return S.Server.LoadFileFromCache("/Pages/404.html");
         }
     }
 }
