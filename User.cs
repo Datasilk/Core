049d35b0062b7942a895417001a8df722f81839a
Mark Entingh <entingh@gmail.com>
2018-05-18 14:31:14 -0700

    minor changes to user class

--- User.cs
+++ User.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
